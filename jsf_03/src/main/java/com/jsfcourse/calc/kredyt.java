c387fb59d246d8e62ab68598f88b14f40b08971f
DanielKnich <127796798+DanielKnich@users.noreply.github.com>
2023-10-31 00:25:21 +0100

    Add files via upload

---
+++ jsf_03/src/main/java/com/jsfcourse/calc/kredyt.java
@@ -0,0 +1,74 @@
+package com.jsfcourse.calc;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.annotation.ManagedProperty;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+@ViewScoped
+//@SessionScoped
+public class kredyt implements Serializable{
+	private Double x; //kwota
+	private Double y; //data
+	private Double z; //procent
+	private Double result; //rata miesięczna
+	
+	@Inject
+	@ManagedProperty("#{txtCalcErr}")
+	private ResourceBundle txtCalcErr;
+	
+	@Inject
+	@ManagedProperty("#{txtCalc}")
+	private ResourceBundle txtCalc;
+
+	@Inject
+	FacesContext ctx;
+
+	public Double getX() {
+		return x;
+	}
+
+	public void setX(Double x) {
+		this.x = x;
+	}
+
+	public Double getY() {
+		return y;
+	}
+
+	public void setY(Double y) {
+		this.y = y;
+	}
+	public Double getZ() {
+		return z;
+	}
+
+	public void setZ(Double z) {
+		this.z = z;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+	
+	public String calc() {
+		result = (x+(x*(z/100))*y)/(y*12);
+		
+		ctx.addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, txtCalcErr.getString("calcComputationOkInfo"), null));
+		ctx.addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, txtCalc.getString("result") + ": " + result, null));
+		return null;
+	}
+}
